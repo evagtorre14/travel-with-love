450bc90744ec7acd3fbc5afeb08b47796324982f
Eva Garcia <C8VMT@ferrovial.com>
2025-08-03 13:16:51 +0200

    feat: add "tiempo"

---
+++ src/data/destination-images.ts
@@ -0,0 +1,15 @@
+import gjirokasterImage from '../assets/gjirokaster.jpg';
+import rivieraImage from '../assets/RivieraAlbanesa.jpg';
+import shkoderImage from '../assets/shkoder.jpg';
+import beratImage from '../assets/berat.jpg';
+import thethImage from '../assets/theth_albania.jpg';
+import tiranaImage from '../assets/tirana.jpeg';
+
+export const destinationImages = {
+  gjirokaster: gjirokasterImage,
+  riviera: rivieraImage,
+  shkoder: shkoderImage,
+  berat: beratImage,
+  theth: thethImage,
+  tirana: tiranaImage,
+};
---
+++ src/data/destinations.ts
@@ -0,0 +1,18 @@
+export const destinations = [
+  {
+    name: 'Gjirokastër',
+    lat: 40.076,
+    lon: 20.138,
+    imageKey: 'gjirokaster',
+  },
+  {
+    name: 'Riviera Albanesa',
+    lat: 40.15,
+    lon: 19.75,
+    imageKey: 'riviera',
+  },
+  { name: 'Shkodër', lat: 42.066, lon: 19.512, imageKey: 'shkoder' },
+  { name: 'Berat', lat: 40.705, lon: 19.952, imageKey: 'berat' },
+  { name: 'Theth', lat: 42.395, lon: 19.774, imageKey: 'theth' },
+  { name: 'Tirana', lat: 41.3275, lon: 19.8187, imageKey: 'tirana' },
+];
