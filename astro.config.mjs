08f3c0524205265c3a8fd226bbc866344bffa58d
Eva Garcia <C8VMT@ferrovial.com>
2025-08-05 14:40:50 +0200

    refactor: increae permormance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://evagtorre14.io',
 	base: '/travel-with-love',
-	integrations: [mdx(), sitemap(), tailwind(), icon()],
+	integrations: [mdx(), sitemap(), tailwind(), icon(), compressor()],
 	image: {
 		service: sharpImageService(),
 	},
